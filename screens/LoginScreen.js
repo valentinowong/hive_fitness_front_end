82444949854c4d18a8e8a6e1f7a3502a5a7c5cfc
Valentino Wong <valentino.wong@gmail.com>
2019-10-13 18:02:25 -0400

    Attempted to get Access Token from Auth0

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,6 +4,8 @@ import { AuthSession } from 'expo';
 import jwtDecode from 'jwt-decode';
 import {AsyncStorage} from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
+import * as Random from 'expo-random';
+import * as Crypto from 'expo-crypto';
 
 const auth0ClientId = 'eM2nUD4q6hiYYmUEQxkkdDCJ0y5tSAq4';
 const auth0Domain = 'https://hivefitness-dev.auth0.com';
@@ -54,17 +56,42 @@ export default class LoginScreen extends React.Component {
       }
     }
 
+
     login = async () => {
+
       // Retrieve the redirect URL, add this to the callback URL list
       // of your Auth0 application.
       const redirectUrl = AuthSession.getRedirectUrl();
       console.log(`Redirect URL: ${redirectUrl}`);
-      
+
+      // Create a Code Verifier
+      base64URLEncode = (str) => {
+        return str.toString('base64')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=/g, '');
+      }
+    
+      verifier = base64URLEncode(await Random.getRandomBytesAsync(32));
+
+      // Create a Code Challenge
+      sha256 = async (buffer) => {
+        const digest = await Crypto.digestStringAsync(
+          Crypto.CryptoDigestAlgorithm.SHA256,
+          buffer
+        );
+        console.log('Digest: ', digest);
+        return digest;
+      }
+      const challenge = base64URLEncode(sha256(verifier));
+
       // Structure the auth parameters and URL
       const queryParams = toQueryString({
+        code_challenge: challenge,
+        code_challenge_method: 'S256',
         client_id: auth0ClientId,
         redirect_uri: redirectUrl,
-        response_type: 'id_token', // id_token will return a JWT token
+        response_type: 'code', // id_token will return a JWT token
         scope: 'openid profile', // retrieve the user's profile
         nonce: 'nonce', // ideally, this will be a random value
       });
@@ -72,27 +99,45 @@ export default class LoginScreen extends React.Component {
   
       // Perform the authentication
       const response = await AuthSession.startAsync({ authUrl });
-      console.log('Authentication response', response);
+      console.log('Authentication response', response, 'Verifier', verifier);
   
       if (response.type === 'success') {
-        this.handleResponse(response.params);
+        if (response.params.error) {
+          Alert('Authentication error', response.error_description || 'something went wrong');
+          return;
+        }
+  
+        // Request Tokens using Authorization Code
+        const code = response.params.code;
+        fetch(`${auth0Domain}/oauth/token`,{
+          method: "POST",
+          header: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          data: JSON.stringify({
+            grant_type: 'authorization_code',
+            client_id: auth0ClientId,
+            code_verifier: verifier,
+            code: code,
+            redirect_uri: redirectUrl
+          })
+        }).then(res=>res.json())
+        .then(data=>console.log('Response Fetch',data))
+  
+    
+        // Retrieve the JWT token and decode it
+        const jwtToken = response.params.id_token;
+        const decoded = jwtDecode(jwtToken);
+    
+        const { name } = decoded;
+        this.setState({ name });
+  
+        this.storeData('token', jwtToken);
       }
     };
   
     handleResponse = (response) => {
-      if (response.error) {
-        Alert('Authentication error', response.error_description || 'something went wrong');
-        return;
-      }
-  
-      // Retrieve the JWT token and decode it
-      const jwtToken = response.id_token;
-      const decoded = jwtDecode(jwtToken);
-  
-      const { name } = decoded;
-      this.setState({ name });
-
-      this.storeData('token', jwtToken);
+      
     };
 
     logout = async () => {
